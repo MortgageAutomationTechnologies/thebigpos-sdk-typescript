5519385db7ab5f25fa7225a0445aac059156804c
Dan Alvare <danalvare@gmail.com>
2024-12-24 09:09:42 -0500

    Updating build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -281,7 +281,7 @@ export interface BusinessRuleRequest {
      */
     name: string;
     description?: string | null;
-    tasks: TaskRequest[];
+    tasks: TaskUpdateRequest[];
     filter: BusinessRuleCondition[];
     applyToAllBorrowerPairs: boolean;
 }
@@ -2521,6 +2521,12 @@ export interface Task {
     willAutocompleteAfterResponse: boolean;
     hasAutoPropagationOnAdd: boolean;
 }
+export interface TaskPaginated {
+    rows: Task[];
+    pagination: Pagination;
+    /** @format int64 */
+    count: number;
+}
 export interface TaskRequest {
     /** @minLength 1 */
     name: string;
@@ -2542,6 +2548,24 @@ export interface TaskSearchCriteria {
     isBusinessRule?: boolean | null;
     isGlobal?: boolean | null;
     excludedIds?: string[] | null;
+}
+export interface TaskUpdateRequest {
+    /** @minLength 1 */
+    name: string;
+    description?: string | null;
+    losTarget?: string | null;
+    type: string;
+    targetUserRole?: string | null;
+    /**
+     * @format int32
+     * @min 0
+     */
+    daysDueFromApplication?: number | null;
+    isGlobal: boolean;
+    willAutocompleteAfterResponse: boolean;
+    hasAutoPropagationOnAdd: boolean;
+    /** @format uuid */
+    id: string;
 }
 export interface TestSendNotificationForLoanRequest {
     loanData: Record<string, string>;
@@ -2956,7 +2980,7 @@ export declare class HttpClient<SecurityDataType = unknown> {
 }
 /**
  * @title The Big POS API
- * @version v2.12.0
+ * @version v2.12.1
  * @termsOfService https://www.thebigpos.com/terms-of-use/
  * @contact Mortgage Automation Technologies <support@thebigpos.com> (https://www.thebigpos.com/terms-of-use/)
  */
@@ -4702,7 +4726,7 @@ export declare class Api<SecurityDataType extends unknown> extends HttpClient<Se
          * @request GET:/api/loans/{loanID}/tasks/diff
          * @secure
          */
-        getLoanTaskDifference: (loanId: string, params?: RequestParams) => Promise<AxiosResponse<UserLoanTask, any>>;
+        getLoanTaskDifference: (loanId: string, params?: RequestParams) => Promise<AxiosResponse<UserLoanTask[], any>>;
         /**
          * No description
          *
@@ -5337,7 +5361,7 @@ export declare class Api<SecurityDataType extends unknown> extends HttpClient<Se
             pageNumber?: number;
             sortBy?: string;
             sortDirection?: string;
-        }, params?: RequestParams) => Promise<AxiosResponse<Task[], any>>;
+        }, params?: RequestParams) => Promise<AxiosResponse<TaskPaginated, any>>;
         /**
          * No description
          *
